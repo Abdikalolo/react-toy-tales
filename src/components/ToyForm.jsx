676a64be6609dc4ddef05295ae05bbcf626596ce
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-11 10:38:02 +0300

    created an iput for submit,added evn handler on the image

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,29 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ onAddToy }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newToy = {
+      name: name,
+      image: image,
+      likes: 0,
+    };
+
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newToy),
+    })
+      .then((response) => response.json())
+      .then((toy) => {
+        onAddToy(toy);
+        setName("");
+        setImage("");
+      })
+      .catch((error) => console.error(error));
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
+
         <input
-          type="text"
+          className="input-text"
           name="name"
           placeholder="Enter a toy's name..."
-          className="input-text"
-        />
-        <br />
-        <input
           type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+
+        <br />
+
+        <input
+          className="input-text"
           name="image"
           placeholder="Enter a toy's image URL..."
-          className="input-text"
+          type="text"
+          value={image}
+          onChange={(event) => setImage(event.target.value)}
         />
+
         <br />
+
         <input
-          type="submit"
-          name="submit"
-          value="Create New Toy"
           className="submit"
+          name="submit"
+          type="submit"
+          value="Create New Toy"
         />
       </form>
     </div>
