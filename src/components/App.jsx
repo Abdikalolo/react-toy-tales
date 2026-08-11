ffdfcac26a1fc4d09ebbd7f785ad61e4c1726a02
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-11 10:33:12 +0300

    created a toyform,buttonclick,delete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,73 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((response) => response.json())
+      .then((data) => setToys(data))
+      .catch((error) => console.error(error));
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function addToy(newToy) {
+    setToys((currentToys) => [...currentToys, newToy]);
+  }
+
+  function deleteToy(id) {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        setToys((currentToys) =>
+          currentToys.filter((toy) => toy.id !== id)
+        );
+      })
+      .catch((error) => console.error(error));
+  }
+
+  function likeToy(toy) {
+    const updatedLikes = toy.likes + 1;
+
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        likes: updatedLikes,
+      }),
+    })
+      .then((response) => response.json())
+      .then((updatedToy) => {
+        setToys((currentToys) =>
+          currentToys.map((currentToy) =>
+            currentToy.id === updatedToy.id ? updatedToy : currentToy
+          )
+        );
+      })
+      .catch((error) => console.error(error));
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+
+      {showForm ? <ToyForm onAddToy={addToy} /> : null}
+
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+
+      <ToyContainer
+        toys={toys}
+        onDeleteToy={deleteToy}
+        onLikeToy={likeToy}
+      />
     </>
   );
 }
