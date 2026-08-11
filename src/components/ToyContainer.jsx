c19efc67bce58b5e0189a201e84f21c3f64605a6
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-11 10:34:37 +0300

    created a function to return mapped toys if liked or deleted

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,19 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
-  const [toys, setToys] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3001/toys")
-      .then((response) => response.json())
-      .then((data) => setToys(data));
-  }, []);
-
+function ToyContainer({ toys, onDeleteToy, onLikeToy }) {
   return (
     <div id="toy-collection">
       {toys.map((toy) => (
-        <ToyCard key={toy.id} toy={toy} />
+        <ToyCard
+          key={toy.id}
+          toy={toy}
+          onDeleteToy={onDeleteToy}
+          onLikeToy={onLikeToy}
+        />
       ))}
     </div>
   );
